d85b9e37957249572a4ead6c150b3cd4ecf8523b
Your Name <you@example.com>
2023-10-26 12:55:13 +0400

    Updated

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView, Modal, Button, TouchableHighlight, TouchableWithoutFeedback, TouchableHighlightBase } from 'react-native';
 import Task from './components/Task';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import * as FileSystem from 'expo-file-system';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 export default function App() {
-  const [taskName, setTaskName] = useState();
+  const [taskName, setTaskName] = useState('');
+  const [taskDeadline, setTaskDeadline] = useState(new Date());
   const [toDoTasks, setToDoTasks] = useState([]);
   const [inProgressTasks, setInProgressTasks] = useState([]);
   const [doneTasks, setDoneTasks] = useState([]);
@@ -13,6 +15,14 @@ export default function App() {
   const [taskType, setTaskType] = useState('todo');
   const [taskColor, setTaskColor] = useState('orange');
   const [allTasks, setAllTasks] = useState({ toDoTasks, inProgressTasks, doneTasks });
+  const [isDialogVisible, setDialogVisible] = useState(false);
+  const [warning, setWarning] = useState('');
+  const [showWarning, setShowWarning] = useState(false);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [editing, setEditing] = useState(false);
+  const [toBeEditedTaskIndex, setToBeEditedTaskIndex] = useState();
+  const [tasksChanged, setTasksChanged] = useState(false);
   const allTasksPath = `${FileSystem.documentDirectory}/all-tasks.json`;
   const isInitialRender = useRef(true);
 
@@ -23,6 +33,21 @@ export default function App() {
       isInitialRender.current = false;
     }
   }, [allTasks]);
+
+  //
+  useEffect(() => {
+    console.log(selectedTasks);
+  }, [selectedTasks]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   useEffect(() => {
     async function loadAllTasks() {
@@ -71,50 +96,129 @@ export default function App() {
   const handleAddTask = () => {
     Keyboard.dismiss();
 
-    const newTask = {
-      type: 'todo',
-      name: taskName,
-      additionTime: getCurrentTime(),
-    };
+    if (taskName.trim() === '') {
+      showWarningMessage('Task name cannot be empty');
+    } else {
+      const newTask = {
+        type: 'todo',
+        name: taskName,
+        deadline: taskDeadline.toLocaleDateString(),
+      };
 
-    setSelectedTasks([...selectedTasks, newTask]);
-    setToDoTasks([...toDoTasks, newTask]);
-    setAllTasks({ ...allTasks, toDoTasks: [...toDoTasks, newTask] })
-    setTaskName('');
+      setSelectedTasks([...selectedTasks, newTask]);
+      setToDoTasks([...toDoTasks, newTask]);
+      setAllTasks({ ...allTasks, toDoTasks: [...toDoTasks, newTask] })
+      setTaskName('');
+      setTaskDeadline(new Date());
+      closeDialog();
+    }
   };
 
-  const handleTasksChange = (array, type, color) => {
-    setSelectedTasks(array);
-    setTaskType(type);
-    setTaskColor(color);
+  const handleEditTask = (index) => {
+    const selectedTask = selectedTasks[index];
+
+    const dateParts = selectedTask.deadline.split('/');
+    const month = parseInt(dateParts[0]) - 1;
+    const day = parseInt(dateParts[1]);
+    const year = parseInt(dateParts[2]);
+    const dateObject = new Date(year, month, day);
+
+    setTaskName(selectedTask.name);
+    setTaskDeadline(dateObject);
+    setTaskType(selectedTask.type);
+
+    setEditing(true);
+    setToBeEditedTaskIndex(index);
+    setDialogVisible(true);
   };
 
-  const moveTask = (type, index) => {
-    const movedTask = selectedTasks[index];
+  const handleSaveEditedTask = () => {
+    const taskIndex = toBeEditedTaskIndex;
+    const selectedTask = selectedTasks[taskIndex];
     let updatedTasks = {};
 
-    setSelectedTasks((prevArray) => prevArray.filter((_, i) => i !== index));
+    const updatedTask = {
+      ...selectedTask,
+      name: taskName,
+      deadline: taskDeadline.toLocaleDateString(),
+    };
 
-    switch (type) {
+    setSelectedTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks];
+      updatedTasks[taskIndex] = updatedTask;
+      return updatedTasks;
+    });
+
+    switch (selectedTask.type) {
+      case 'todo':
+        const updatedToDoTasks = [...toDoTasks];
+        updatedToDoTasks[taskIndex] = updatedTask;
+        setToDoTasks(updatedToDoTasks);
+
+        updatedTasks = {
+          toDoTasks: updatedToDoTasks,
+          inProgressTasks,
+          doneTasks,
+        };
+
+        break;
+      case 'inprogress':
+        const updatedInProgressTasks = [...inProgressTasks];
+        updatedInProgressTasks[taskIndex] = updatedTask;
+        setInProgressTasks(updatedInProgressTasks);
+
+        updatedTasks = {
+          toDoTasks,
+          inProgressTasks: updatedInProgressTasks,
+          doneTasks,
+        };
+
+        break;
+      case 'done':
+        const updatedDoneTasks = [...doneTasks];
+        updatedDoneTasks[taskIndex] = updatedTask;
+        setDoneTasks(updatedDoneTasks);
+
+        updatedTasks = {
+          toDoTasks,
+          inProgressTasks,
+          doneTasks: updatedDoneTasks,
+        };
+
+        break;
+      default:
+        break;
+    }
+
+    setAllTasks((prevAllTasks) => ({ ...prevAllTasks, ...updatedTasks }));
+    setEditing(false);
+    closeDialog();
+  };
+
+  const handleDeleteTask = (index) => {
+    const selectedTask = selectedTasks[index];
+    let updatedTasks = {};
+
+    setSelectedTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
+
+    switch (selectedTask.type) {
       case 'todo':
         setToDoTasks((prevArray) => prevArray.filter((_, i) => i !== index));
-        setInProgressTasks([...inProgressTasks, { ...movedTask, type: 'inprogress', additionTime: getCurrentTime() }]);
 
         updatedTasks = {
           toDoTasks: toDoTasks.filter((_, i) => i !== index),
-          inProgressTasks: [...inProgressTasks, { ...movedTask, type: 'inprogress', additionTime: getCurrentTime() }],
+          inProgressTasks,
           doneTasks,
         };
 
         break;
       case 'inprogress':
         setInProgressTasks((prevArray) => prevArray.filter((_, i) => i !== index));
-        setDoneTasks([...doneTasks, { ...movedTask, type: 'done', additionTime: getCurrentTime() }]);
 
         updatedTasks = {
           toDoTasks,
           inProgressTasks: inProgressTasks.filter((_, i) => i !== index),
-          doneTasks: [...doneTasks, { ...movedTask, type: 'done', additionTime: getCurrentTime() }],
+          doneTasks,
         };
 
         break;
@@ -135,15 +239,111 @@ export default function App() {
     setAllTasks((prevAllTasks) => ({ ...prevAllTasks, ...updatedTasks }));
   };
 
+  const handleInfoTask = (index) => {
+    const selectedTask = selectedTasks[index];
+
+    alert(`Name: ${selectedTask.name}\n\nDeadline: ${selectedTask.deadline}\n\nTask type: ${selectedTask.type}`);
+  };
+
+  const handleTasksChange = (array, type, color) => {
+    setSelectedTasks(array);
+    setTaskType(type);
+    setTaskColor(color);
+    setTasksChanged(true);
+  };
+
+  const moveTaskForward = (type, index) => {
+    const movedTask = selectedTasks[index];
+    let updatedTasks = {};
+
+    setSelectedTasks((prevArray) => prevArray.filter((_, i) => i !== index));
+
+    switch (type) {
+      case 'todo':
+        setToDoTasks((prevArray) => prevArray.filter((_, i) => i !== index));
+        setInProgressTasks([...inProgressTasks, { ...movedTask, type: 'inprogress' }]);
+
+        updatedTasks = {
+          toDoTasks: toDoTasks.filter((_, i) => i !== index),
+          inProgressTasks: [...inProgressTasks, { ...movedTask, type: 'inprogress' }],
+          doneTasks,
+        };
+
+        break;
+      case 'inprogress':
+        setInProgressTasks((prevArray) => prevArray.filter((_, i) => i !== index));
+        setDoneTasks([...doneTasks, { ...movedTask, type: 'done' }]);
+
+        updatedTasks = {
+          toDoTasks,
+          inProgressTasks: inProgressTasks.filter((_, i) => i !== index),
+          doneTasks: [...doneTasks, { ...movedTask, type: 'done' }],
+        };
+
+        break;
+      default:
+        break;
+    }
+
+    setAllTasks((prevAllTasks) => ({ ...prevAllTasks, ...updatedTasks }));
+  };
+
+  const moveTaskBackward = (type, index) => {
+    const movedTask = selectedTasks[index];
+    let updatedTasks = {};
+
+    setSelectedTasks((prevArray) => prevArray.filter((_, i) => i !== index));
+
+    switch (type) {
+      case 'inprogress':
+        setInProgressTasks((prevArray) => prevArray.filter((_, i) => i !== index));
+        setToDoTasks([...toDoTasks, { ...movedTask, type: 'todo' }]);
+
+        updatedTasks = {
+          toDoTasks: [...toDoTasks, { ...movedTask, type: 'todo' }],
+          inProgressTasks: inProgressTasks.filter((_, i) => i !== index),
+          doneTasks,
+        };
+
+        break;
+      case 'done':
+        setDoneTasks((prevArray) => prevArray.filter((_, i) => i !== index));
+        setInProgressTasks([...inProgressTasks, { ...movedTask, type: 'inprogress' }]);
+
+        updatedTasks = {
+          toDoTasks,
+          inProgressTasks: [...inProgressTasks, { ...movedTask, type: 'inprogress' }],
+          doneTasks: doneTasks.filter((_, i) => i !== index),
+        };
+
+        break;
+      default:
+        break;
+    }
+
+    setAllTasks((prevAllTasks) => ({ ...prevAllTasks, ...updatedTasks }));
+  };
+
+  const closeDialog = () => {
+    setTaskName('');
+    setTaskDeadline(new Date());
+
+    if (editing) {
+      setEditing(false);
+    }
+
+    setDialogVisible(false);
+  };
+
   const getCurrentDate = () => {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     var hours = new Date().getUTCHours();
     var date = new Date().getUTCDate();
     var month = months[new Date().getUTCMonth()];
     var year = new Date().getUTCFullYear();
-    var day = days[new Date().getUTCDay() - 1];
+    var day = days[new Date().getUTCDay()];
 
     if (hours + 4 > 23) {
       date += 1;
@@ -152,27 +352,30 @@ export default function App() {
     return date + ' ' + month + ' ' + year + ', ' + day;
   };
 
-  const getCurrentTime = () => {
-    var hours = new Date().getUTCHours();
-    var minutes = new Date().getUTCMinutes();
-    var period = 'AM';
-
-    if (hours + 4 >= 12) {
-      period = 'PM';
-    }
-    else if (hours + 4 >= 0) {
-      period = 'AM';
-    }
-
-    if (hours + 4 > 23) {
-      hours = hours + 4 - 24
-    }
-    else {
-      hours += 4
-    }
-
-    return hours + ':' + minutes + ' ' + period;
+  const hideDatePicker = () => {
+    setShowDatePicker(false);
   };
+
+  const handleDateConfirm = (date) => {
+    setTaskDeadline(date);
+    hideDatePicker();
+  };
+
+  const showWarningMessage = (message) => {
+    setWarning(message);
+    setShowWarning(true);
+
+    setTimeout(() => {
+      setShowWarning(false);
+      setWarning('');
+    }, 3000);
+  };
+
+  const handleTasksChanged = () => {
+    setTasksChanged(false);
+  };
+
+  //
 
   return (
     <View style={styles.container}>
@@ -182,14 +385,17 @@ export default function App() {
         }}
         keyboardShouldPersistTaps='handled'
       >
+        {showWarning && (
+          <View style={styles.warning}>
+            <Text style={styles.warningMessage}>{warning}</Text>
+          </View>
+        )}
+
         <View style={styles.tasksWrapper}>
           <View style={styles.tasksHeader}>
             <Text style={styles.sectionTitle}>Today</Text>
-            <Icon style={styles.sectionIcon}
-              name="list"
-              size={25}
-              color='gray'
-            />
+
+            <Text style={styles.currentTime}>{currentTime.toLocaleTimeString()}</Text>
           </View>
 
           <Text style={styles.sectionDate}>{getCurrentDate()}</Text>
@@ -218,9 +424,7 @@ export default function App() {
             {
               selectedTasks.map((task, index) => {
                 return (
-                  <TouchableOpacity key={index} onPress={() => moveTask(task.type, index)}>
-                    <Task name={task.name} additionTime={task.additionTime} color={taskColor} />
-                  </TouchableOpacity>
+                  <Task key={index} index={index} type={task.type} name={task.name} deadline={task.deadline} color={taskColor} tasksChanged={tasksChanged} handleTasksChanged={handleTasksChanged} moveForward={moveTaskForward} moveBackward={moveTaskBackward} handleEdit={handleEditTask} handleDelete={handleDeleteTask} handleInfo={handleInfoTask} />
                 )
               })
             }
@@ -228,21 +432,66 @@ export default function App() {
         </View>
       </ScrollView>
 
-      {taskType == 'todo' && (
+      {(taskType == 'todo' || editing) && (
         <KeyboardAvoidingView
           behavior={Platform.OS === "ios" ? "padding" : "height"}
           style={styles.writeTaskWrapper}
         >
-          <TextInput style={styles.input} placeholder={'Task name'} value={taskName} onChangeText={text => setTaskName(text)} />
-          <TouchableOpacity onPress={() => handleAddTask()}>
-            <View style={styles.addWrapper}>
-              <Icon
-                name="plus"
-                size={30}
-                color="white"
-              />
+          {!editing && (
+            <TouchableOpacity onPress={() => setDialogVisible(true)}>
+              <View style={styles.addWrapper}>
+                <Icon
+                  name="plus"
+                  size={35}
+                  color="grey"
+                />
+              </View>
+            </TouchableOpacity>
+          )}
+
+          <Modal visible={isDialogVisible} transparent={true}
+            animationType='fade' >
+            <View style={styles.centeredDialog}>
+              <View style={styles.dialogView}>
+                <Text style={{ fontSize: 28, fontWeight: '500' }}>New Task</Text>
+
+                <TextInput
+                  value={taskName}
+                  onChangeText={(text) => setTaskName(text)}
+                  placeholder="Task name"
+                  style={styles.dialogInput}
+                />
+
+                <View style={styles.datePicker}>
+                  <TouchableOpacity onPress={() => setShowDatePicker(true)}>
+                    <Icon
+                      name="calendar"
+                      size={30}
+                      color="grey"
+                    />
+                  </TouchableOpacity>
+
+                  <DateTimePickerModal
+                    isVisible={showDatePicker}
+                    mode="date"
+                    minimumDate={new Date()}
+                    onConfirm={handleDateConfirm}
+                    onCancel={hideDatePicker}
+                  />
+                  <Text style={{ fontSize: 22, fontWeight: '500', color: 'gray' }}>{taskDeadline.toLocaleDateString()}</Text>
+                </View>
+
+                <View style={styles.dialogButtons}>
+                  <TouchableHighlight style={styles.dialogButton} onPress={editing ? handleSaveEditedTask : handleAddTask} underlayColor="lightgrey">
+                    <Text style={styles.buttonText}>Save</Text>
+                  </TouchableHighlight>
+                  <TouchableHighlight style={[styles.dialogButton, { backgroundColor: 'orangered' }]} onPress={closeDialog} underlayColor="lightgrey">
+                    <Text style={styles.buttonText}>Cancel</Text>
+                  </TouchableHighlight>
+                </View>
+              </View>
             </View>
-          </TouchableOpacity>
+          </Modal>
         </KeyboardAvoidingView>
       )}
     </View>
@@ -255,7 +504,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#f1f2f4',
   },
   tasksWrapper: {
-    paddingTop: 60,
+    paddingTop: 40,
     paddingHorizontal: 20,
   },
   tasksHeader: {
@@ -267,14 +516,17 @@ const styles = StyleSheet.create({
     fontSize: 60,
     fontWeight: '700',
   },
-  sectionIcon: {
-    marginTop: 10,
+  currentTime: {
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: '500',
+    color: 'grey'
   },
   sectionDate: {
     marginTop: 10,
-    fontSize: 18,
-    fontWeight: '700',
-    color: '#999999'
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#999'
   },
   taskTypes: {
     marginTop: 30,
@@ -284,7 +536,7 @@ const styles = StyleSheet.create({
   },
   taskType: {
     paddingVertical: 15,
-    paddingHorizontal: 20,
+    paddingHorizontal: 18,
     borderRadius: 60,
     borderColor: '#C0C0C0',
     borderWidth: 1,
@@ -296,34 +548,95 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 30,
     width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center'
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 25,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 300,
-    fontSize: 22,
-    fontWeight: '700',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: 'forestgreen',
+    width: 70,
+    height: 70,
+    backgroundColor: '#fff',
     borderRadius: 60,
     justifyContent: 'center',
     alignItems: 'center',
     borderColor: '#C0C0C0',
-    borderWidth: 1,
+    borderWidth: 5,
   },
   typeText: {
     color: '#fff',
-    fontSize: 22,
-    fontWeight: '700'
+    fontSize: 24,
+    fontWeight: '600'
+  },
+  centeredDialog: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)'
+  },
+  dialogView: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    padding: 20,
+    gap: 20,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  dialogButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  dialogButton: {
+    width: '47%',
+    padding: 10,
+    backgroundColor: 'green',
+    borderRadius: 20,
+  },
+  dialogInput: {
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 10,
+    fontSize: 23,
+  },
+  buttonText: {
+    fontSize: 25,
+    fontWeight: '600',
+    color: 'white',
+    textAlign: 'center',
+  },
+  datePicker: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    paddingVertical: 3,
+  },
+  warning: {
+    backgroundColor: 'red',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 13,
+    position: 'absolute',
+    top: 60,
+    right: 20,
+    zIndex: 100,
+  },
+  warningMessage: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '500',
   }
 });
--- components/Task.js
+++ components/Task.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, TouchableHighlight } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Task = (props) => {
+  const [isOptionsVisible, setOptionsVisible] = useState(false);
+  const [isChanged, setIsChanged] = useState(true);
+  const { tasksChanged, handleTasksChanged } = props;
+
+  const checkDeadline = () => {
+    currentDate = new Date().toLocaleDateString();
+
+    if (props.deadline < currentDate) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  const handleIsChanged = () => {
+    setIsChanged(false);
+  }
+
+  useEffect(() => {
+    alert('ok')
+    
+    setIsChanged(tasksChanged);
+    console.log(tasksChanged);
+  }, []);
+
+  useEffect(() => {
+    console.log(isChanged);
+  }, [isChanged]);
+
+  const showHideOptions = () => {
+    // handleTasksChanged();
+    handleIsChanged();
+    setOptionsVisible(!isOptionsVisible);
+  };
+
   return (
     <View style={styles.item}>
-      <View style={styles.itemLeft}>
+      <View style={styles.itemUp}>
         <View style={styles.circle}>
           <Icon
             name="circle"
@@ -14,11 +49,53 @@ const Task = (props) => {
           />
         </View>
 
-        <Text style={styles.itemText}>{props.name}</Text>
+        <View style={styles.itemLeft}>
+          <Text style={styles.itemText}>{props.name}</Text>
+
+          <Text style={[styles.deadline, { color: checkDeadline() ? 'green' : 'red' }]}>{props.deadline}</Text>
+        </View>
+
+        <TouchableWithoutFeedback onPress={() => showHideOptions()}>
+          <Text style={styles.taskMore}>...</Text>
+        </TouchableWithoutFeedback>
+
+        <View style={styles.itemRight}>
+          <TouchableOpacity onPress={() => props.moveBackward(props.type, props.index)} disabled={props.type === 'todo'}>
+            <View style={props.type !== 'todo' ? [styles.arrowWrapper, { paddingRight: 5 }] : [styles.arrowWrapper, { opacity: 0.5, paddingRight: 5 }]}>
+              <Icon
+                name="chevron-left"
+                size={25}
+                color="grey"
+              />
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => props.moveForward(props.type, props.index)} disabled={props.type === 'done'}>
+            <View style={props.type !== 'done' ? [styles.arrowWrapper, { paddingLeft: 5 }] : [styles.arrowWrapper, { opacity: 0.5, paddingLeft: 5 }]}>
+              <Icon
+                name="chevron-right"
+                size={25}
+                color="grey"
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
 
-      <Text style={styles.additionTime}>{props.additionTime}</Text>
-    </View>
+      {(isOptionsVisible && !isChanged) &&
+        <View style={styles.itemDown}>
+          <TouchableHighlight onPress={() => props.handleEdit(props.index)} style={[styles.taskOption, { backgroundColor: 'deepskyblue' }]} underlayColor="lightgrey">
+            <Text style={styles.optionText}>Edit</Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => props.handleDelete(props.index)} style={[styles.taskOption, { backgroundColor: 'orangered' }]} underlayColor="lightgrey">
+            <Text style={styles.optionText}>Delete</Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => props.handleInfo(props.index)} style={[styles.taskOption, { backgroundColor: 'forestgreen' }]} underlayColor="lightgrey">
+            <Text style={styles.optionText}>Info</Text>
+          </TouchableHighlight>
+        </View>
+      }
+    </View >
   )
 }
 
@@ -28,33 +105,73 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 20,
     borderRadius: 15,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
     marginBottom: 25,
+    gap: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 5,
   },
+  itemUp: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  itemDown: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   itemLeft: {
+    justifyContent: 'space-between',
+    gap: 10,
+    width: '55%',
+  },
+  itemRight: {
     flexDirection: 'row',
     alignItems: 'center',
-    maxWidth: '64%',
+    justifyContent: 'space-between',
+    width: '32%',
+  },
+  taskMore: {
+    position: 'absolute',
+    top: -35,
+    right: -7,
+    fontSize: 30,
+    fontWeight: '800',
+  },
+  deadline: {
+    fontSize: 20,
+    fontWeight: '500',
   },
   circle: {
-    paddingTop: 3,
-    marginRight: 20,
+    paddingTop: 12,
   },
   itemText: {
-    fontSize: 25,
+    fontSize: 27,
     fontWeight: '600',
   },
-  additionTime: {
-    fontSize: 15,
+  taskOption: {
+    width: '31%',
+    padding: 7,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+  optionText: {
+    fontSize: 22,
     fontWeight: '500',
-    color: '#999999',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  arrowWrapper: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 4,
   },
 });
 
